a71ae09f4d7da9dca8b31952e90bdc4505a7fae2
unknown <gmdin716@gmail.com>
2022-12-02 23:39:12 +0500

    handling userInputState and validation

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,19 +1,41 @@
 import Card from "../UI/Card/Card";
 import Button from "../UI/Button/Button";
 import classes from './InputForm.module.css';
+import { useState } from "react";
 const InputForm=(props)=>{
+const [name,setName]=useState('');
+const [age,setAge]=useState('');
+    const handleNameField=event=>{
+            setName(event.target.value);
+    }
+    const handleAgefield=event=>{
+            setAge(event.target.value);
+    }
 
     const submintHandler=event=>{
         event.preventDefault();
+        if(name==='' && age===''){
+            alert("Username and age must be required");
+        }
+        else if(name===''){
+            alert('Username must be entered');
+        }
+        else if(age===''){
+            alert('Age must be entered');
+        }
+        else{
+            setAge('');
+            setName('');
+        }
     }
 
     return(
         <Card className={classes.input}>
         <form onSubmit={submintHandler}>
             <label htmlFor='username' >Username</label>
-            <input type='text' id="username"/>
+            <input type='text' id="username" value={name} onChange={handleNameField}/>
             <label htmlFor="age">Age(years)</label>
-            <input type='number' id="age"/>
+            <input type='number' id="age" value={age} onChange={handleAgefield}/>
             <Button type="submit">Add user</Button>
         </form>
         </Card>
